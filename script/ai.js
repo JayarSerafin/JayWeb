e0ba03db2552242e4a4207ab7ba0340a7bcb3fe4
JayarDev0 <168594571+JayarDev0@users.noreply.github.com>
2024-05-01 09:28:35 +0800

    Create ai.js

---
+++ script/ai.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+module.exports.config = {
+    name: "ai",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "Jay", // Changed the credits to "Jay"
+    description: "EDUCATIONAL",
+    usePrefix: true,
+    commandCategory: "AI",
+    usages: "[question]",
+    cooldowns: 10
+};
+
+module.exports.run = async function ({ api, event, args }) {
+    const question = args.join(' ');
+    const apiUrl = `https://markdevsapi-2014427ac33a.herokuapp.com/gpt4?ask=${encodeURIComponent(question)}`;
+
+    if (!question) return api.sendMessage("Please provide a question first.", event.threadID, event.messageID);
+
+    try {
+        api.sendMessage("Jay Ar Is Thinking Please Wait...", event.threadID, event.messageID);
+
+        const response = await axios.get(apiUrl);
+        const answer = response.data.answer;
+
+        api.sendMessage(`𝗔𝗜 🚀\n━━━━━━━━━━━━━━━━━━━\n𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ${question}\n━━━━━━━━━━━━━━━━━━━\n𝗔𝗻𝘀𝘄𝗲𝗿: ${answer}\n\nthis bot was create by Jay Ar pogi\n𝓒𝓻𝓮𝓭𝓲𝓽𝓼: https://www.facebook.com/profile.php?id=61552691907450`, event.threadID, event.messageID); // Added the FB link
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while processing your request.", event.threadID);
+    }
+};
