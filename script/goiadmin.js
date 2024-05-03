54e7c37417c20e169b99376e27de23dba3385948
JayarDev0 <168594571+JayarDev0@users.noreply.github.com>
2024-05-03 11:10:07 +0800

    Update goiadmin.js

--- script/goiadmin.js
+++ script/goiadmin.js
@@ -12,19 +12,19 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event }) {
     const mentionedNames = Object.values(event.mentions).map(user => user.toLowerCase());
     
-    if (mentionedNames.includes("churchill")) {
+    if (mentionedNames.includes("Jay")) {
         var msg = [
-            "Babe nalang iatawag mo sakanya",
+            "Busy Nigga!!",
             "Stop mentioning my creator, he's busy 😗",
             "My Creator is currently offline 😢",
             "𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
             "busy pa ata yun kaya mag-antay ka",
-            "Sorry, naka bebetime pa don't disturb him 🙄",
+            "Please Don't Mention my creator",
             "Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/profile.php?id=100087212564100",
             "Another tag in my Creator, i will kick your fucking ass"
         ];
 
-        api.setMessageReaction("😍", event.messageID, (err) => {}, true);
+        api.setMessageReaction("🌐", event.messageID, (err) => {}, true);
         return api.sendMessage({ body: msg[Math.floor(Math.random()*msg.length)] }, event.threadID, event.messageID);
     }
 };
