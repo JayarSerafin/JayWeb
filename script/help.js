4657ef2f37a25fc9ff7f3db691958df064f9b811
JayarDev0 <168594571+JayarDev0@users.noreply.github.com>
2024-05-15 19:57:30 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -29,13 +29,13 @@ module.exports.run = async function({
             let end = start + pages;
             let helpMessage = `\n\n====『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧: 』====\n\n`;
             for (let i = start; i < Math.min(end, commands.length); i++) {
-                helpMessage += `  │ ✧\n  | {commands[i]}\n  \n`;
+                helpMessage += `  \n  |  ${prefix}${commands[i]}\n  \n`;
             }
             helpMessage += '\n====『 𝗘𝗩𝗘𝗡𝗧 𝗟𝗜𝗦T: 』====\n\n';
             eventCommands.forEach((eventCommand, index) => {
-                helpMessage += `  │ ✧\n  |   {eventCommand}\n  \n`;
+                helpMessage += `  \n  | 『 ${index + 1}.』  ${prefix}${eventCommand}\n   \n`;
             });
-            helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help 2'. To view information about a specific command, type '${prefix}help command name' Developer : facebook.com/61552691907450.\n\n`;
+            helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help 2'. To view information about a specific command, type '${prefix}help command name'.\n\n`;
             api.sendMessage(helpMessage, event.threadID, event.messageID);
         } else if (!isNaN(input)) {
             if (input === '2') {
@@ -45,7 +45,7 @@ module.exports.run = async function({
                 let end = start + pages;
                 let helpMessage = `\n\n====『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧: 』====\n\n`;
                 for (let i = start; i < Math.min(end, commands.length); i++) {
-                    helpMessage += `  ╭─❍\n  | 『 ${i + 1}.』  ${prefix}${commands[i]}\n │ ✧  \n`;
+                    helpMessage += `  \n  |  ${prefix}${commands[i]}\n  \n`;
                 }
                 helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the previous page, type '${prefix}help'. To view information about a specific command, type '${prefix}help command name'.\n\n`;
                 api.sendMessage(helpMessage, event.threadID, event.messageID);
@@ -74,4 +74,4 @@ module.exports.handleEvent = async function({
     if (body?.toLowerCase().startsWith('prefix')) {
         api.sendMessage(message, threadID, messageID);
     }
-}
+                    }
